b5a42130b65cfdb20e5d9b8ed7acf00b5bb83ee7
Letam <davidsonletam@gmail.com>
2023-03-30 17:30:34 +0100

    change

--- exercise2/exercise2.1.py
+++ exercise2/exercise2.1.py
@@ -2,7 +2,8 @@ print("Welcome to a centimeter to inch converter")
 
 length_centimeter = eval(input("Enter length in centimeter: "))
 if length_centimeter < 0 :
-    print("Entry is invalid")
+    print("Entry is invalid, try again")
 else:
     length_inch = length_centimeter * 2.54
     print("Length in inches is", length_inch)
+
