de482dfe66080f9dc3346b458fd4f8766e780454
Letam <davidsonletam@gmail.com>
2023-03-30 17:18:04 +0100

    first commit

---
+++ exercise1/exercise1.6.py
@@ -0,0 +1,2 @@
+x = eval(input("Enter a number: "))
+print(x * 1, x * 2, x * 3, x * 4, x * 5, sep='---')
---
+++ exercise1/exercise1.7.py
@@ -0,0 +1,4 @@
+print("Welcome to a kilogram to pounds converter")
+weight_in_kg = eval(input("Enter weight in Kilogram: "))
+weight_in_pounds = weight_in_kg * 2.2
+print(weight_in_kg,"kg", "is", weight_in_pounds, "pounds")
